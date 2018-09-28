3236d3ba92f7e79d15b4fbc6cb96427d08ebaccd
1052145436@qq.com <1052145436@qq.com>
2018-09-28 14:27:08 +0800

    【修改】供应商逻辑修改。

--- static/js/supplier.js
+++ static/js/supplier.js
@@ -10,13 +10,14 @@ var supplierObj = {
             initSocailBox();
 
             function initSocailBox(){
-                $('#socail-nav>li').on('click', function () {
+                $('#socail-nav>li').click(function(){
                     $('#socail-nav>li').each(function () {
                         $(this).removeClass('current');
                     });
 
                     $(this).addClass('current');
                     $('#social-nav-bar').stop().animate({'left': (60 + $(this).index() * $(this).width()) + 'px'}, 500)
+                    $('#scroll-ul').stop().animate({'left': -($(this).index() * 1200) + 'px'}, 500)
                 })
             }
         })
