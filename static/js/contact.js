2a830fcdf8a5698ce1cdc1a52460a8b559d0d4bd
1052145436@qq.com <1052145436@qq.com>
2018-09-28 18:06:52 +0800

    【修改】供应商逻辑修改。

--- static/js/contact.js
+++ static/js/contact.js
@@ -7,6 +7,31 @@ var contactObj = {
             commonJS.commonObj.init();
             commonJS.commonObj.initLazyload();
             commonJS.commonObj.defaultChooseNav(5);
+
+
+            initUI.apply(this);
+
+            function initUI() {
+                // 百度地图API功能
+                var map = new BMap.Map("allmap");
+                var point = new BMap.Point(113.3640940000,23.1039830000);
+                map.centerAndZoom(point, 18);
+                var marker = new BMap.Marker(point);  // 创建标注
+                map.addOverlay(marker);               // 将标注添加到地图中
+                marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
+                map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+
+                var top_left_control = new BMap.ScaleControl({anchor: BMAP_ANCHOR_TOP_LEFT});// 左上角，添加比例尺
+                var top_left_navigation = new BMap.NavigationControl();  //左上角，添加默认缩放平移控件
+
+                //添加控件和比例尺
+                function add_control(){
+                    map.addControl(top_left_control);
+                    map.addControl(top_left_navigation);
+
+                };
+                add_control();
+            }
         })
     }
 }
